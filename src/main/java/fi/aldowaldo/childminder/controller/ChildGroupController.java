74c8a2d050401f6b57fe9744e9567f82ec384844
brodewa <waltteri.broder@cinia.fi>
2019-07-16 07:54:47 +0300

    CrossOrigin

--- src/main/java/fi/aldowaldo/childminder/controller/ChildGroupController.java
+++ src/main/java/fi/aldowaldo/childminder/controller/ChildGroupController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/childgroup")
 public class ChildGroupController {
 
